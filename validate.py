bb57019af7587497632ea3bc9fdc1ebf8f6e884c
Martin Monperrus <martin.monperrus@gnieh.org>
2017-06-25 10:14:09 +0200

    add validation about duplicates

--- validate.py
+++ validate.py
@@ -1,8 +1,10 @@
 import json
+import re
 from  jsonschema import validate
 
 data = json.load(open('crawler-user-agents.json'))
 
+# check for format using JSON Schema
 schema = {
     "type": "array",
     "items": {
@@ -13,5 +15,18 @@ schema = {
         "required" : [ "pattern" ]
     }
 }
-
 validate(data, schema)
+
+# check for duplicates
+class Entry: # we need this to be able to use i!=j with exact same content
+    pass
+data2 = []
+for i in data:
+    x=Entry()
+    x.pattern = i['pattern'].lower()
+    data2.append(x)
+for i in data2:
+    for j in data2:
+        if i!=j and re.match(i.pattern, j.pattern, re.IGNORECASE):
+            raise Exception('duplicate '+i.pattern+' '+j.pattern)
+
