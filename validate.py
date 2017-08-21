fd80ab7ad2d4b73d100fd01c06ed37490f1d7ed6
Martin Monperrus <martin.monperrus@gnieh.org>
2017-08-21 21:35:44 +0200

    improve validation script

--- validate.py
+++ validate.py
@@ -24,9 +24,22 @@ data2 = []
 for i in data:
     x=Entry()
     x.pattern = i['pattern'].lower()
+    x.instances = i['instances'] if "instances" in i else []
     data2.append(x)
+
+nbinstances = 0
 for i in data2:
+    # contract #1 do we match the given instances?
+    for instance in i.instances:
+        nbinstances += 1
+        assert re.search(i.pattern, instance, re.IGNORECASE), i.pattern+" does not match "+instance
+            
+        # ongoing work by @vetty
+        # assert re2.match(i.pattern, instance, re.IGNORECASE)
+        
     for j in data2:
-        if i!=j and re.match(i.pattern, j.pattern, re.IGNORECASE):
+        if i!=j and re.search(i.pattern, j.pattern, re.IGNORECASE):
             raise Exception('duplicate '+i.pattern+' '+j.pattern)
+
+assert nbinstances>10
 
