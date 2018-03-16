d714dfaf3ea53d7e5be374e8727f560231c044b3
Martin Monperrus <martin.monperrus@gnieh.org>
2018-03-16 19:52:02 +0100

    testing giant regexp in PHP

--- validate.php
+++ validate.php
@@ -3,10 +3,24 @@
 
 $data = json_decode(file_get_contents('crawler-user-agents.json'), true);
 
+$patterns = array();
 foreach($data as $entry) {
+  $patterns[] = $entry['pattern'];
   if (isset($entry['instances'])) {
     foreach($entry['instances'] as $ua_example) {
       if (!preg_match('/'.$entry['pattern'].'/', $ua_example)) {
+        throw new Exception('pb with '.$entry['pattern']);
+      }
+    }
+  }
+}
+
+// testing with a giant regexp
+$regexp = implode('|', $patterns);
+foreach($data as $entry) {
+  if (isset($entry['instances'])) {
+    foreach($entry['instances'] as $ua_example) {
+      if (!preg_match('/'.$regexp.'/', $ua_example)) {
         throw new Exception('pb with '.$entry['pattern']);
       }
     }
