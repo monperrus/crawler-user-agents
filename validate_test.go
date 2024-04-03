eacd6fd0fa29e0d233fca842fc073aa7356dfb58
Boris Nagaev <bnagaev@gmail.com>
2024-04-03 16:15:33 -0300

    golang: add benchmark for MatchingCrawlers

--- validate_test.go
+++ validate_test.go
@@ -17,10 +17,18 @@ func TestPatterns(t *testing.T) {
 	}
 }
 
-func BenchmarkPatterns(b *testing.B) {
+func BenchmarkIsCrawler(b *testing.B) {
 	userAgent := "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 Google-PageRenderer Google (+https://developers.google.com/+/web/snippet/)"
 	b.SetBytes(int64(len(userAgent)))
 	for n := 0; n < b.N; n++ {
 		IsCrawler(userAgent)
+	}
+}
+
+func BenchmarkMatchingCrawlers(b *testing.B) {
+	userAgent := "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 Google-PageRenderer Google (+https://developers.google.com/+/web/snippet/)"
+	b.SetBytes(int64(len(userAgent)))
+	for n := 0; n < b.N; n++ {
+		MatchingCrawlers(userAgent)
 	}
 }
