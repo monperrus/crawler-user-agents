c88b5d384168a59b7519647e684084d85c9ecac9
Ilia Gandelman <ilia@joyned.co>
2024-12-17 14:43:41 +0200

    Feature: make case sensitivity optional

--- __init__.py
+++ __init__.py
@@ -1,6 +1,23 @@
 import re
 import json
+from functools import cached_property
 from pathlib import Path
+
+
+class CrawlerPatterns:
+    def __init__(self):
+        pass
+
+    @cached_property
+    def case_insensitive(self):
+        return re.compile(
+            "|".join(i["pattern"] for i in CRAWLER_USER_AGENTS_DATA),
+            re.IGNORECASE
+        )
+
+    @cached_property
+    def case_sensitive(self):
+        return re.compile("|".join(i["pattern"] for i in CRAWLER_USER_AGENTS_DATA))
 
 
 def load_json():
@@ -11,24 +28,24 @@ def load_json():
 
 
 CRAWLER_USER_AGENTS_DATA = load_json()
-CRAWLER_USER_AGENTS_REGEXP = re.compile(
-    "|".join(i["pattern"] for i in CRAWLER_USER_AGENTS_DATA)
-)
+CRAWLER_PATTERNS = CrawlerPatterns()
 
 
-def is_crawler(user_agent: str) -> bool:
+def is_crawler(user_agent: str, case_sensitive: bool = True) -> bool:
     """Return True if the given User-Agent matches a known crawler."""
-    return bool(CRAWLER_USER_AGENTS_REGEXP.search(user_agent))
+    if case_sensitive:
+        return bool(re.search(CRAWLER_PATTERNS.case_sensitive, user_agent))
+    return bool(re.search(CRAWLER_PATTERNS.case_insensitive, user_agent))
 
 
-def matching_crawlers(user_agent: str) -> list[int]:
+def matching_crawlers(user_agent: str, case_sensitive: bool = True) -> list[int]:
     """
     Return a list of the indices in CRAWLER_USER_AGENTS_DATA of any crawlers
     matching the given User-Agent.
     """
     result = []
-    if is_crawler(user_agent):
+    if is_crawler(user_agent, case_sensitive):
         for num, crawler_user_agent in enumerate(CRAWLER_USER_AGENTS_DATA):
-            if re.search(crawler_user_agent["pattern"], user_agent):
+            if re.search(crawler_user_agent["pattern"], user_agent, 0 if case_sensitive else re.IGNORECASE):
                 result.append(num)
     return result
